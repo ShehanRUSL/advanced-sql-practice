97129065cee8c33807ffdcf6f865b60aa9399bbc
Shehan Hasantha Chandrasiri <shehanhasantha10@gmail.com>
2026-08-11 21:51:57 +0530

    upload sql files

---
+++ project_sql/3_top_demanded_skills.sql
@@ -0,0 +1,17 @@
+
+
+
+select 
+skills,
+count(skills_job_dim.job_id) as job_count
+FROM job_postings_fact
+INNER JOIN skills_job_dim
+    ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim
+    ON skills_job_dim.skill_id = skills_dim.skill_id
+where job_title_short = 'Data Analyst' and
+job_work_from_home = True
+   
+   
+    group by skills
+    order by demand_count desc
